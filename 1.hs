8f56cb14f2a9adfffadb1762e054f8382db23ec2
Diego G <diegogalvis0905@hotmail.com>
2022-10-28 13:36:52 -0500

    Without promedio done

--- 1.hs
+++ 1.hs
@@ -1,17 +1,25 @@
 import Data.List
 
-solveRPN :: String -> Float
+condNumero :: Float -> Float
+condNumero x 
+  | x == 3 = 100
+  | x == 5 = 25
+  | otherwise = 0
+
+sumPromedio :: [Float] -> Float
+sumPromedio x = sum x / round (length x)
+
+solveRPN :: String -> Float  
 solveRPN = head . foldl foldingFunction [] . words
-
-  where 
-    foldingFunction (x:y:ys) "*" = (x * y):ys
-    foldingFunction (x:y:ys) "+" = (x + y):ys
-    foldingFunction (x:y:ys) "-" = (y - x):ys
-    foldingFunction (x:y:ys) "/" = (y / x):ys
-    foldingFunction (x:xs) "neg1" = (x - x*2): xs
-    foldingFunction (x:xs) "sqrt" = (sqrt x) : xs
-    foldingFuction (x:xs) "condNumero" = (condNumero x):xs
-    foldingFunction xs "sum" = [sum xs]
-    foldingFunction xs numberString = read numberString:xs
-
+  where   foldingFunction (x:y:ys) "+" = (x + y):ys
+          foldingFunction (x:y:ys) "-" = (y - x):ys
+          foldingFunction (x:y:ys) "*" = (x * y):ys
+          foldingFunction (x:y:ys) "/" = (y / x):ys
+          foldingFunction (x:xs) "neg1" = (x - x*2): xs
+          foldingFunction (x:xs) "sqrt" = (sqrt x) : xs
+          foldingFunction (x:xs) "condNumero" = (condNumero x):xs
+          foldingFunction xs "sumaTotal" = [sum xs]
+          foldingFunction xs "producto" = [product xs]
+          foldingFunction x "promedio" = [sumPromedio x]
+          foldingFunction xs numberString = read numberString:xs
 
